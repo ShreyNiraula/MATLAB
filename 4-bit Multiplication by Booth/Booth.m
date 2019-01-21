5d62bb4938b376b88fa25c4767390931d229c254
Shrey53 <nshrey53@gmail.com>
2019-01-21 19:13:24 +0545

    Booth method, generalized for n-bits

--- 4-bit Multiplication by Booth/Booth.m
+++ 4-bit Multiplication by Booth/Booth.m
@@ -10,11 +10,9 @@ function [] = Booth(M,Q,n)
     for i = 2:n+1
         M(i)=input('Enter the n bit multiplicand:');
     end
-    
     for i = 2:n+1
         Q(i)=input('Enter the n bit multiplier:');
     end
-    
     
     M(1)=0;
     Q(1)=0;
@@ -24,79 +22,50 @@ function [] = Booth(M,Q,n)
     for i = 1:n+1
         N(i)=xors(1,M(i));
     end
-    
     carry=1; 
-   
     for i=n+1:-1:1
         [N(i), carry]= fullAdder(N(i), A(i), carry);
     end
     
     % takes n+1 cycles for n-bits
     for i=1:n+1
-        if((Q(n+1)==0 && q==0)||(Q(n+1)==1 && q==1))
-            % just arithmetic shift right of AQq
-            q=Q(n+1);
-            for i = n+1:-1:2
-                Q(i)=Q(i-1);
-            end
-
-            Q(1)=A(n+1);
-            
-            for i = n+1:-1:2
-            A(i)=A(i-1);
-            end
-           % retain A(1)= MSB as it is 
         
-        
-        elseif((Q(n+1)==1 && q==0))
+        if((Q(n+1)==1 && q==0))
             % do A=A-M => A=A+N (as, N=-M)
             carry=0; % internal carry for A=A+N(i)
             for i=n+1:-1:1
                 [A(i) , carry] = fullAdder(A(i), N(i), carry);
             end
-            
-            % now ASR
-            q=Q(n+1);
-            for i = n+1:-1:2
-                Q(i)=Q(i-1);
-            end
-
-            Q(1)=A(n+1);
-            
-            for i = n+1:-1:2
-            A(i)=A(i-1);
-            end
-           % retain A(1)= MSB as it is
-           
+          
         elseif((Q(n+1)==0 && q==1))
             % do A=A+M 
             carry=0; % internal carry for A=A+M(i)
             for i=n+1:-1:1
                 [A(i) , carry] = fullAdder(A(i), M(i), carry);
             end
-            
-            % now ASR
-            q=Q(n+1);
-            for i = n+1:-1:2
-                Q(i)=Q(i-1);
-            end
-
-            Q(1)=A(n+1);
-            
-            for i = n+1:-1:2
-            A(i)=A(i-1);
-            end
-           % retain A(1)= MSB as it is
-            
         end
+      
+        % now ASR for all cases
+        q=Q(n+1);
+        for i = n+1:-1:2
+            Q(i)=Q(i-1);
+        end
+        
+        Q(1)=A(n+1);
+        
+        for i = n+1:-1:2
+            A(i)=A(i-1);
+        end
+       % retain A(1)= MSB as it is
     end
+    
     % at last, check if MSB of AQ=1, if 1, -ve result, so 2's complements
     if(A(1)==1)
         %do 2's of AQ
         M(n+1)=zeros;  % M no longer used, reset to 0
         for i = 1:n+1
-        A(i)=xors(1,A(i));
-        Q(i)=xors(1,Q(i));
+            A(i)=xors(1,A(i));
+            Q(i)=xors(1,Q(i));
         end
 
         carryA=1; 
